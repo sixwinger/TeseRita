83b73f2f0faa0c7c0e339315a8c4e16086b026b8
sixwinger <obadiah.hakeswill.gold@protonmail.com>
2022-03-05 20:50:33 +0000

    20220305 Mutação

---
+++ BackUp/20220305/Input.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import math as Math
+print('A juntar informação Inicial')
+
+def funcVarAlogritmo():
+
+    dictVariavelAlgoritmo={
+
+        'tamanho_Pop': 84,              #Tamanho da população
+        'taxa_Mutacao': 0.025           #Taxa de mutação
+    }
+
+    return dictVariavelAlgoritmo
+
+def funcConstrangimentos():
+
+    dictConstrangimentos={
+        'Vcx' : 0.5,                    #Volumetria da caixa em m3
+        'Volume_Max': 50,               #Volume maximo                
+        'vel_Carrinha' : 1 ,            #Velocidade em km/m da carrinha
+        'tempo_entrega_volume' : 2,     #Tempo de entrega por volume
+        'hora_Arranque' : 615,          #Hora de arranque
+    }
+
+    return dictConstrangimentos
+
+def funcDadosExcel(caminhoFicheiroDistancias,caminhoFicheiroVolumes):
+
+    mDistancias = pd.read_excel (caminhoFicheiroDistancias)
+    mVolumes = pd.read_excel (caminhoFicheiroVolumes)
+
+    return mDistancias, mVolumes 
+
+if __name__ == "__main__":
+
+    caminhoFicheiroDistancias ='Dados/Distancias.xlsx' 
+    caminhoFicheiroVolumes = 'Dados/Volumes.xlsm' 
+    mDistancias, mVolumesDados = funcDadosExcel(caminhoFicheiroDistancias, caminhoFicheiroVolumes)
+    dictConstrangimentos = funcConstrangimentos()
+    dictVariavelAlgoritmo = funcVarAlogritmo()
+
+    print('Input Main')
---
+++ BackUp/20220305/Mutacao.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from random import randrange
+
+def funcMutancao(cromossoma):
+
+   print(cromossoma)
+   i = randrange(len(cromossoma)-1)
+   j = randrange(i+1,len(cromossoma))
+   novoCromossoma = cromossoma.copy()
+   print(str(i) + ":" + str(j))
+
+   while i<j:
+
+      print(cromossoma[i])
+      print(cromossoma[j])
+      novoCromossoma[i] = cromossoma[j] 
+      novoCromossoma[j] = cromossoma[i] 
+
+      i+=1      
+      j-=1
+
+   print(novoCromossoma)
+   return novoCromossoma
+
+
+
+if __name__ == "__main__":
+
+
+
+    cromossoma = [3, 9, 5, 2, 6, 4, 1, 7, 8]
+
+
+    filho = funcMutancao(cromossoma)
+
+    print('-- Fim Mutação --')   
