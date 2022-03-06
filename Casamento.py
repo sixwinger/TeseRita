0bd463b7121002672c9262dd15521df8487d46e2
sixwinger <obadiah.hakeswill.gold@protonmail.com>
2022-03-06 01:14:49 +0000

    20220306 Mutanção e Casamento

--- Casamento.py
+++ Casamento.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from AEXcruzamento import *
+from Mutacao import *
 from random import random
+
 def funcSelecaoPais(mFitnessGeral):
 
     valor = random()
@@ -13,20 +16,30 @@ def funcSelecaoPais(mFitnessGeral):
             break 
     return pai
 
-def funCasamento(pais,mPop):
+def funCasamento(pais,mPop, dictVariavelAlgoritmo):
 
-    pai = mPop[pais[0]]
-    mae = mPop[pais[1]]
+    pais ={
+        
+        'pai' : mPop[pais[0]].to_list(),
+        'mae' : mPop[pais[1]].to_list()
+       }
 
-    return cromossoma
+    filho = funcAexcruzamento(pais)
+    taxa_Mutacao = randrange(1000)/1000
+    if taxa_Mutacao < dictVariavelAlgoritmo['taxa_Mutacao']:
+        print(taxa_Mutacao)
+        filho = funcMutancao(filho)
 
+    return filho
 
+# juntar um filho numa matrix e criar a função mae
 if __name__ == "__main__":
 
-    
+    from Input import *    
+    dictVariavelAlgoritmo = funcVarAlogritmo()
     mFitnessGeral = pd.read_excel ('Dados/Debug/Cromossoma_debug.xlsx', sheet_name='Fitness Resultado', index_col=0)
     mPop = pd.read_excel ('Dados/Debug/Data_debug.xlsx', index_col=0)
     pais = [funcSelecaoPais(mFitnessGeral),funcSelecaoPais(mFitnessGeral)]
-    funCasamento(pais, mPop)
+    funCasamento(pais, mPop, dictVariavelAlgoritmo)
     print (pais)
     print ("-- Casamento Fim --")
