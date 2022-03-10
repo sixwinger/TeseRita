dbd4ae9da69b11ad1d33e369d42a2b02aa42b5cb
sixwinger <obadiah.hakeswill.gold@protonmail.com>
2022-03-10 21:09:11 +0000

    20220310 Confirmação Mutação e Cruzamento AEX

--- Mutacao.py
+++ Mutacao.py
@@ -6,6 +6,7 @@ def funcMutancao(cromossoma):
 
    i = randrange(len(cromossoma)-1)
    j = randrange(i+1,len(cromossoma))
+   print(str(i)+':'+str(j))
    novoCromossoma = cromossoma.copy()
 
    while i<j:
@@ -20,7 +21,8 @@ def funcMutancao(cromossoma):
 
 if __name__ == "__main__":
 
-    cromossoma = [3, 9, 5, 2, 6, 4, 1, 7, 8]
-    filho = funcMutancao(cromossoma)
+   cromossoma = [3, 9, 5, 2, 6, 4, 1, 7, 8, 15, 14, 13 ,12, 11, 10]
+   filho = funcMutancao(cromossoma)
+   print(filho)
 
-    print('-- Fim Mutação --')   
+   print('-- Fim Mutação --')   
